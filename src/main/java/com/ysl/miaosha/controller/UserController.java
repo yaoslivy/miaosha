7cc03e3e7caf5548ae5415bb71f34fa448d6b58f
yaoshuilin <42492091+yaoslivy@users.noreply.github.com>
2022-07-06 15:26:40 +0800

    feat(error,response): 定义了通用返回对象-返回正确/错误信息，定义了异常处理解决类

--- src/main/java/com/ysl/miaosha/controller/UserController.java
+++ src/main/java/com/ysl/miaosha/controller/UserController.java
@@ -2,29 +2,43 @@ package com.ysl.miaosha.controller;
 
 
 import com.ysl.miaosha.controller.viewobject.UserVO;
+import com.ysl.miaosha.error.BusinessException;
+import com.ysl.miaosha.error.EmBusinessError;
+import com.ysl.miaosha.response.CommonReturnType;
 import com.ysl.miaosha.service.UserService;
 import com.ysl.miaosha.service.model.UserModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller("user")
 @RequestMapping("/user")
-public class UserController {
+public class UserController extends BaseController{
     @Autowired
     UserService userService;
 
     @RequestMapping("/get")
     @ResponseBody //使用在单独的方法上的，需要哪个方法返回json数据格式，就在哪个方法上使用
-    public UserVO getUser(@RequestParam(name="id") Integer id) {
+    public CommonReturnType getUser(@RequestParam(name="id") Integer id) throws BusinessException {
         //调用service服务获取对应id的用户对象并返回给前端
         UserModel userModel = userService.getUserById(id);
+
+        //若获取的对应用户信息不存在
+        if(userModel == null){
+//             userModel.setEncrptPassword("123");
+            throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
+        }
+
         //将核心领域模型用户对象转化为可供UI使用的viewobject
-        return convertFromModel(userModel);
+        UserVO userVO = convertFromModel(userModel);
+        //返回通用对象
+        return CommonReturnType.create(userVO);
     }
 
     //将model对象转换为OV对象返回
@@ -36,6 +50,8 @@ public class UserController {
         BeanUtils.copyProperties(userModel,userVO);
         return userVO;
     }
+
+
 
 
 }
