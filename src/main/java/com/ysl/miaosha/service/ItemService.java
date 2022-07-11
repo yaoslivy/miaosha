96e9c4ad743953f59ac9ddd86f8ed8481ed170ad
yaoshuilin <42492091+yaoslivy@users.noreply.github.com>
2022-07-11 14:16:07 +0800

    feat(item): 完成商品模型和实现
    1. 完成商品创建功能
    2. 完成商品列表展示
    3. 完成商品详情页展示

---
+++ src/main/java/com/ysl/miaosha/service/ItemService.java
@@ -0,0 +1,23 @@
+package com.ysl.miaosha.service;
+
+import com.ysl.miaosha.error.BusinessException;
+import com.ysl.miaosha.service.model.ItemModel;
+
+import java.util.List;
+
+public interface ItemService {
+    //创建商品
+    ItemModel createItem(ItemModel itemModel) throws BusinessException;
+
+    //商品列表浏览
+    List<ItemModel> listItem();
+//
+    //商品详情浏览
+    ItemModel getItemById(Integer id);
+//
+//    //库存扣减
+//    boolean decreaseStock(Integer itemId,Integer amount)throws BusinessException;
+//
+//    //商品销量增加
+//    void increaseSales(Integer itemId,Integer amount)throws BusinessException;
+}
---
+++ src/main/java/com/ysl/miaosha/service/impl/ItemServiceImpl.java
@@ -0,0 +1,121 @@
+package com.ysl.miaosha.service.impl;
+
+import com.ysl.miaosha.dao.ItemDOMapper;
+import com.ysl.miaosha.dao.ItemStockDOMapper;
+import com.ysl.miaosha.dataobject.ItemDO;
+import com.ysl.miaosha.dataobject.ItemStockDO;
+import com.ysl.miaosha.error.BusinessException;
+import com.ysl.miaosha.error.EmBusinessError;
+import com.ysl.miaosha.service.ItemService;
+import com.ysl.miaosha.service.model.ItemModel;
+import com.ysl.miaosha.validator.ValidationResult;
+import com.ysl.miaosha.validator.ValidatorImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+    @Autowired
+    private ValidatorImpl validator;
+    @Autowired
+    private ItemStockDOMapper itemStockDOMapper;
+    @Autowired
+    private ItemDOMapper itemDOMapper;
+
+
+    @Override
+    public List<ItemModel> listItem() {
+        List<ItemDO> itemDOList = itemDOMapper.listItem();
+
+        List<ItemModel> itemModelList = itemDOList.stream().map(itemDO -> {
+            ItemStockDO itemStockDO = itemStockDOMapper.selectByItemId(itemDO.getId());
+            ItemModel itemModel = this.convertModelFromDataObject(itemDO, itemStockDO);
+            return itemModel;
+        }).collect(Collectors.toList());
+
+        return itemModelList;
+    }
+
+    @Override
+    @Transactional
+    public ItemModel createItem(ItemModel itemModel) throws BusinessException {
+        //检验入参
+        ValidationResult result = validator.validate(itemModel);
+        if(result.isHasErrors()){
+            throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,result.getErrMsg());
+        }
+        //转化itemmodel->dataobject
+        ItemDO itemDO = this.convertItemDOFromItemModel(itemModel);
+        //写入数据库
+        itemDOMapper.insertSelective(itemDO);
+        itemModel.setId(itemDO.getId());
+
+        ItemStockDO itemStockDO = this.convertItemStockDOFromItemModel(itemModel);
+        itemStockDOMapper.insertSelective(itemStockDO);
+        //返回创建完成的对象
+        return this.getItemById(itemModel.getId());
+    }
+
+
+
+    private ItemDO convertItemDOFromItemModel(ItemModel itemModel){
+        if(itemModel == null){
+            return null;
+        }
+        ItemDO itemDO = new ItemDO();
+        BeanUtils.copyProperties(itemModel,itemDO);
+        itemDO.setPrice(itemModel.getPrice().doubleValue());
+        return itemDO;
+    }
+    private ItemStockDO convertItemStockDOFromItemModel(ItemModel itemModel){
+        if(itemModel == null){
+            return null;
+        }
+        ItemStockDO itemStockDO = new ItemStockDO();
+        itemStockDO.setItemId(itemModel.getId());
+        itemStockDO.setStock(itemModel.getStock());
+        return itemStockDO;
+    }
+
+
+
+    public ItemModel getItemById(Integer id) {
+        ItemDO itemDO = itemDOMapper.selectByPrimaryKey(id);
+        if(itemDO == null){
+            return null;
+        }
+        //操作获得库存数量
+        ItemStockDO itemStockDO = itemStockDOMapper.selectByItemId(itemDO.getId());
+
+
+        //将dataobject->model
+        ItemModel itemModel = convertModelFromDataObject(itemDO,itemStockDO);
+
+        //获取活动商品信息
+//        PromoModel promoModel = promoService.getPromoByItemId(itemModel.getId());
+//        if(promoModel != null && promoModel.getStatus().intValue() != 3){
+//            itemModel.setPromoModel(promoModel);
+//        }
+        return itemModel;
+    }
+
+    private ItemModel convertModelFromDataObject(ItemDO itemDO,ItemStockDO itemStockDO){
+        ItemModel itemModel = new ItemModel();
+        BeanUtils.copyProperties(itemDO,itemModel);
+        itemModel.setPrice(new BigDecimal(itemDO.getPrice()));
+        itemModel.setStock(itemStockDO.getStock());
+
+        return itemModel;
+    }
+
+
+
+
+}
