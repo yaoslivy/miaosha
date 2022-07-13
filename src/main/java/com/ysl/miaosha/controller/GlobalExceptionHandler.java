7e61b5f3d733302ea9e79693733867eb406319b7
yaoshuilin <42492091+yaoslivy@users.noreply.github.com>
2022-07-13 14:46:05 +0800

    fix(error): 修改BaseController捕获异常
    增加全局异常处理类GlobalExceptionHandler（@ControllerAdvice）完成对于404，405问题的处理

---
+++ src/main/java/com/ysl/miaosha/controller/GlobalExceptionHandler.java
@@ -0,0 +1,40 @@
+package com.ysl.miaosha.controller;
+
+import com.ysl.miaosha.error.BusinessException;
+import com.ysl.miaosha.error.EmBusinessError;
+import com.ysl.miaosha.response.CommonReturnType;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public CommonReturnType doError(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,Exception ex) {
+        ex.printStackTrace();
+        Map<String, Object> responseData = new HashMap<>();
+        if (ex instanceof BusinessException) {
+            BusinessException businessException = (BusinessException) ex;
+            responseData.put("errCode", businessException.getErrCode());
+            responseData.put("errMsg", businessException.getErrMsg());
+        } else if (ex instanceof ServletRequestBindingException) {  //405问题 参数必传:localhost:8090/item/get
+            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+            responseData.put("errMsg","url绑定路由问题");
+        } else if (ex instanceof NoHandlerFoundException) {   //404问题 localhost:8090/item/get1?id=6
+            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+            responseData.put("errMsg", "没有找到对应的访问路径");
+        } else {
+            responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
+            responseData.put("errMsg", EmBusinessError.UNKNOWN_ERROR.getErrMsg());
+        }
+        return CommonReturnType.create(responseData, "fail");
+    }
+}
