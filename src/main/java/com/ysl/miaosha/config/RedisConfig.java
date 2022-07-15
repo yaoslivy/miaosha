34ae9c6d8aa49e47d3c9d3be8fab5d25a5465e93
yaoshuilin <42492091+yaoslivy@users.noreply.github.com>
2022-07-15 19:23:04 +0800

    feat(sessionid): 基于token传输类似sessionid：java代码session实现迁移到redis,UserModel需要实现序列化

---
+++ src/main/java/com/ysl/miaosha/config/RedisConfig.java
@@ -0,0 +1,12 @@
+package com.ysl.miaosha.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 3600)
+public class RedisConfig {
+
+}
